710bae3b5093efc564ddce3830733d590f3550f1
Jcarth3w <jdcarthew@bsu.edu>
2022-12-28 13:04:09 -0600

    Add classes. Update Piece class

--- src/main/java/chess/engine/pieces/Piece.java
+++ src/main/java/chess/engine/pieces/Piece.java
@@ -1,6 +1,23 @@
 package chess.engine.pieces;
 
-public class Piece
+import chess.engine.Color;
+import chess.engine.board.Board;
+import chess.engine.board.Move;
+
+import java.util.List;
+
+public abstract class Piece
 {
+    protected final int piecePosition;
+    protected final Color pieceColor;
+
+    Piece(final int piecePosition, final Color pieceColor)
+    {
+        this.piecePosition = piecePosition;
+        this.pieceColor = pieceColor;
+    }
+
+    public abstract List<Move> calculateLegalMoves(final Board board);
+
 
 }
